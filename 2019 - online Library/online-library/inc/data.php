91a5ac16a92b4da9d36557dc336567e39ce8192a
Prof. Muñoz <cmunoz@lagcc.cuny.edu>
2019-12-11 18:29:39 -0500

    Add files via upload

---
+++ 2019 - online Library/online-library/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Beowulf",
+	"img" => "img/media/beowulf.jpg",
+    "genre" => "Epic Poetry",
+    "format" => "Hardcover",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Seamus Heaney"
+    ],
+    "publisher" => "Farrar, Straus and Giroux",
+    "isbn" => '9780374111199'
+];
+$catalog[102] = [
+    "title" => "The Great Gatsby",
+    "img" => "img/media/gatsby.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2004,
+    "category" => "Books",
+    "authors" => [
+        "F. Scott Fitzgerald"
+    ],
+    "publisher" => "Scribner",
+    "isbn" => '9780743273565'
+];
+$catalog[103] = [
+    "title" => "Phantom of the Opera",
+    "img" => "img/media/phantom.jpg",
+    "genre" => "Gothic Fiction",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Gaston Leroux"
+    ],
+    "publisher" => "Barnes & Noble",
+    "isbn" => '9781593082499'
+];
+$catalog[104] = [
+    "title" => "Frankenstein",
+    "img" => "img/media/frankenstein.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "Mary Shelley"
+    ],
+    "publisher" => "Barnes & Noble",
+    "isbn" => '9781593081157'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Kill Bill: Vol 1",
+    "img" => "img/media/Kill.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Streaming",
+    "year" => 2003,
+    "category" => "Movies",
+    "director" => "Quentin Tarantino",
+    "writers" => [
+        "Uma Thurman",
+        "Quentin Tarantino"
+    ],
+    "stars" => [
+        "Uma Thurman",
+        "Lucy Liu",
+        "Sigourney Weaver",
+    ]
+];
+$catalog[202] = [
+    "title" => "Interstellar",
+    "img" => "img/media/Interstellar.jpg",
+    "genre" => "Science Fiction",
+    "format" => "DVD/Streaming",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "Christopher Nolan",
+    "writers" => [
+        "Jonathan Nolan",
+				"Christopher Nolan"
+    ],
+    "stars" => [
+			"Matthew McConaughey",
+			"Jessica Chastain",
+			"Anne Hathaway"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Man From U.N.C.L.E",
+    "img" => "img/media/Uncle.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Guy Ritchie",
+    "writers" => [
+        "Guy Ritchie",
+        "Lionel Wigram"
+    ],
+    "stars" => [
+			"Alicia Vikander",
+			"Henry Cavill",
+			"Armie Hammer"
+    ]
+];
+$catalog[204] = [
+    "title" => "The Martian",
+		"img" => "img/media/martian.jpg",
+	  "genre" => "Drama, Science Fiction, Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 2015,
+	  "category" => "Movies",
+	  "director" => "Ridley Scott",
+	  "writers" => [
+	      "James Cameron"
+
+	  ],
+	  "stars" => [
+	      "Matt Damon",
+	      "Jessica Chastain",
+	      "Kristen Wiig"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Virtue",
+	"img" => "img/media/virtue.jpg",
+	"genre" => "Prison Jazz, Experimental Rock",
+	"format" => "CD",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "The Voidz"
+];
+
+$catalog[302] = [
+	"title" => "Can't Get Over You",
+	"img" => "img/media/joji.jpg",
+	"genre" => "Lofi Hip-Hop",
+	"format" => "Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Joji ft. Clams Casino"
+];
+$catalog[303] = [
+	"title" => "Alive (Live)",
+	"img" => "img/media/alive.jpg",
+	"genre" => "Funk, Electronic",
+	"format" => "CD, Streaming Services",
+	"year" => 2007,
+	"category" => "Music",
+	"artist" => "Daft Punk"
+];
+$catalog[304] = [
+	"title" => "D.A.N.C.E",
+	"img" => "img/media/dance.jpg",
+	"genre" => "Funk, Electronic",
+	"format" => "CD, Streaming Services",
+	"year" => 2007,
+	"category" => "Music",
+	"artist" => "Justice"
+];
+?>
